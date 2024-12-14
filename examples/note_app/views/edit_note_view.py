a8ae969f291a23659eebd7dc2474788f6125cf06
saurabhwadekar <saurabhwadekar420@gmail.com>
2024-12-14 08:49:11 +0530

    get_param use

--- examples/note_app/views/edit_note_view.py
+++ examples/note_app/views/edit_note_view.py
@@ -4,18 +4,18 @@ from fletx import Xview
 class EditNoteView(Xview):
 
     def init(self):
-        self.title = ft.TextField(label="title",value=self.params.get("note_title"))
-        self.note = ft.TextField(label="note",value=self.state.notes[self.params.get("note_title")],multiline=True)
+        self.title = ft.TextField(label="title",value=self.get_param("note_title"))
+        self.note = ft.TextField(label="note",value=self.state.notes[self.get_param("note_title")],multiline=True)
 
     def build(self):
         return ft.View(
             floating_action_button=ft.FloatingActionButton(icon=ft.Icons.SAVE,
-                on_click=lambda e: self.state.updateNote(old_title=self.params.get("note_title"),
+                on_click=lambda e: self.state.updateNote(old_title=self.get_param("note_title"),
                                                          title=self.title.value,
                                                          note=self.note.value)),
             appbar=ft.AppBar(
                 leading=ft.IconButton(icon=ft.icons.ARROW_BACK,on_click=self.back),
-                title=ft.Text(self.params.get("note_title")),
+                title=ft.Text(self.get_param("note_title")),
                 center_title=True,
             ),
             controls=[
