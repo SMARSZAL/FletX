a1555c324918206343ac8232e63d9686fa488ca4
saurabhwadekar <saurabhwadekar420@gmail.com>
2024-12-15 21:25:15 +0530

    v0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = "fletx",
-    version = "0.1.2",
+    version = "0.1.3",
     author="Saurabh Wadekar [ INDIA ]",
     packages=["fletx"],
     license="MIT",
